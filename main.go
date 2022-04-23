c42a2c843fdd0a4701b4b24019a0d4bc9aebdd0a
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-04-23 22:20:41 +0430

    my_cards

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 
-	cards := new deck ()
+	cards := newDeck ()
 
 	fmt.Println(cards.tostring())
 	
 }
+
+cards.savetofile("my_cards")
+
+//If we write in the terminal go run main.go deck.go
+//output = a new file named my_cards
